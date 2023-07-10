dec756f438445c85c22f869ddf2467f8832ca4b5
Adrian Perez Juarez <adrianperezj@gmail.com>
2023-07-09 20:10:21 -0600

    location with average ratings

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,31 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-1.times do
-  User.create(
-    email: Faker::adrianperez@gmail.com,
-    username: Faker::adrianperez,
-    password: "123456",
-    password_confirmation: "123456"
+require "faker"
+
+17.times do
+  Review.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    comment: Faker::Lorem.paragraph(sentence_count: 2),
+    # photo: 'logo.png',
+    noise_level: rand(1..5),
+    family_zone: rand(1..5),
+    pet_friendly: rand(1..5),
+    nightlife: rand(1..5),
+    safety: rand(1..5),
+    internet_speed: rand(1..5),
+    restaurants_qty: rand(1..5),
+    shopping: rand(1..5),
+    meal_cost: Faker::Commerce.price(range: 0..120.0),
+    beer_cost: Faker::Commerce.price(range: 0..20.0),
+    coffee_cost:  Faker::Commerce.price(range: 0..15.0),
+    rent_cost: Faker::Commerce.price(range: 0..10000.0),
+    uber: [true, false].sample,
+    bike: [true, false].sample,
+    metro: [true, false].sample,
+    bus: [true, false].sample,
+    dealer: [true, false].sample,
+    user_id: 1,
+    location_id: rand(1..3)
   )
 end
-
-
