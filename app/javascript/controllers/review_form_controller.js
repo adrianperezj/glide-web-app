16be66d8b08996851ae77e6f01738e0f375ebbd6
Blanca Rodriguez <blancarmp95@gmail.com>
2023-07-17 14:58:19 -0600

    Transitions, Range Inputs and Progress Bar

--- app/javascript/controllers/review_form_controller.js
+++ app/javascript/controllers/review_form_controller.js
@@ -1,64 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["section", "actionButton"]
+  static targets = ["section", "actionButton", "progressBar"]
 
   connect() {
     this.currentSectionIndex = 0
     this.sections = Array.from(this.sectionTargets)
-    //this.updateProgress()
+    this.updateProgress()
   }
-
-
-//   <div data-review-form-target="section" id="step-1">
-//   <h2>General</h2>
-//   <%= form.input :title %>
-//   <%= form.input :comment, as: :text %>
-//   <%= form.input :photo, as: :file %>
-//   <button data-action="click->review-form#transition" data-direction="next">Next ---&gt;</button>
-// </div>
-
-// <div data-review-form-target="section" id="step-2" class="d-none">
-//   <h2>Expenses</h2>
-//   <%= form.input :meal_cost %>
-//   <%= form.input :coffee_cost %>
-//   <%= form.input :rent_cost %>
-//   <%= form.input :beer_cost %>
-//   <button data-action="click->review-form#transition" data-direction="next">Next ---&gt;</button>
-//   <button data-action="click->review-form#transition" data-direction="previous">&lt;--- Previous</button>
-// </div>
-
-// <div data-review-form-target="section" id="step-3" class="d-none">
-//   <h2>Ratings</h2>
-//   <%= form.input :noise_level, collection: 1..5 %>
-//   <%= form.input :safety, collection: 1..5 %>
-//   <%= form.input :internet_speed, collection: 1..5 %>
-//   <%= form.input :family_zone, collection: 1..5 %>
-//   <%= form.input :nightlife, collection: 1..5 %>
-//   <%= form.input :pet_friendly, collection: 1..5 %>
-//   <%= form.input :restaurants_qty, collection: 1..5 %>
-//   <%= form.input :shopping, collection: 1..5 %>
-//   <%= form.input :dealer, as: :boolean %>
-//   <button data-action="click->review-form#transition" data-direction="next">Next ---&gt;</button>
-//   <button data-action="click->review-form#transition" data-direction="previous">&lt;--- Previous</button>
-// </div>
-
-// <div data-review-form-target="section" id="step-4" class="d-none">
-//   <h2>Mobility</h2>
-//   <%= form.input :metro, as: :boolean %>
-//   <%= form.input :bike, as: :boolean %>
-//   <%= form.input :bus, as: :boolean %>
-//   <%= form.input :uber, as: :boolean %>
-//   <button data-action="click->review-form#transition" data-direction="next">Next ---&gt;</button>
-//   <button data-action="click->review-form#transition" data-direction="previous">&lt;--- Previous</button>
-// </div>
-
-// <div data-review-form-target="section" id="step-5" class="d-none">
-//   <h2>Success</h2>
-//   <p>Your review has been submitted successfully.</p>
-//   <%= form.button :submit, class: 'btn btn-primary' %>
-//   <%= link_to 'Go Back', location_path(@location), class: 'btn btn-secondary' %>
-// </div>
 
 
   transition(event) {
@@ -69,7 +18,7 @@ export default class extends Controller {
     this.hideCurrentSection()
     this.showNextSection(nextSectionIndex)
     this.currentSectionIndex = nextSectionIndex
-    // this.updateProgress()
+    this.updateProgress()
   }
 
   hideCurrentSection() {
@@ -92,40 +41,17 @@ export default class extends Controller {
 
   calculateNextSectionIndex(direction) {
     if (direction === "next") {
-      const nextSectionIndex = this.currentSectionIndex + 1;
-      nextSection = this.sections[nextSectionIndex];
+      return this.currentSectionIndex + 1
     } else if (direction === "previous") {
-      const previousSectionIndex = this.currentSectionIndex - 1;
-      nextSection = this.sections[previousSectionIndex];
-    }
-
-    currentSection.classList.add("d-none");
-    nextSection.classList.remove("d-none");
-
-    this.currentSectionIndex = Array.from(this.sections).indexOf(nextSection);
-
-    this.updateProgress();
-    this.updateButtons();
-  }
-
-  updateButtons() {
-    // Show or hide previous and next buttons based on current section
-    if (this.currentSectionIndex === 0) {
-      this.hidePreviousButton();
-    } else if (this.currentSectionIndex === this.sections.length - 1) {
-      this.hideNextButton();
-      this.showSubmitButton();
-    } else {
-      this.showPreviousButton();
-      this.showNextButton();
+      return this.currentSectionIndex - 1
     }
   }
 
-//   //updateProgress() {
-//    // const progress = this.progressTarget
-//     const totalSections = this.sections.length
-//     const completedSections = this.currentSectionIndex + 1
-//     const progressPercentage = (completedSections / totalSections) * 100
-//     progress.style.width = `${progressPercentage}%`
-//   }
-}
+  updateProgress() {
+    const progress = this.progressBarTarget
+    const totalSections = this.sections.length
+    const completedSections = this.currentSectionIndex + 1
+    const progressPercentage = (completedSections / totalSections) * 100
+    progress.style.width = `${progressPercentage}%`
+    }
+  }
