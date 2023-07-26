749a2f8644e57a47a2430c94b4fb564fb8836fd8
Adrian Perez Juarez <adrianperezj@gmail.com>
2023-07-25 21:24:01 -0600

    searchbar fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,11 @@ class PagesController < ApplicationController
 
   def cities
     if params[:query].present?
-      @locations = Location.all.find { |location| location.city == params[:query]} || "City not found"
+      # @locations = Location.all.find { |location| location.city == params[:query]} || "City not found"
+      @locations = Location.search_by_city_and_neighborhood(params[:query]).first
     else
       # return all locations excluding repeated cities, include only the first location with city
       # @locations = Location.all.group_by(&:city).map { |city, locations| locations.first }
-      @locations = Location.all
     end
   end
 
